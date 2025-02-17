83426835960464cdbb04bf19daa66934f485186b
Khasan Atsaev <atsaev.khasan@yandex.ru>
2025-02-18 01:36:29 +0300

    inheritance added. new child class created

--- Employee.py
+++ Employee.py
@@ -24,6 +24,27 @@ class Employee:
                 f'{Employee.vacation_days}.')
 
 
+class FullTimeEmployee(Employee):
+
+    def get_unpaid_vacation(self, start_date, duration):
+        return (f'Начало неоплачиваемого отпуска: {start_date},'
+                f' продолжительность: {duration} дней.')
+
+
+class PartTimeEmployee(Employee):
+
+    vacation_days = 14
+
+    def __init__(self, first_name, second_name, gender):
+        super().__init__(first_name, second_name, gender)
+        self.remaining_vacation_days = PartTimeEmployee.vacation_days
+
+    def get_unpaid_vacation(self, start_date, duration):
+
+        return (f'Начало неоплачиваемого отпуска: {start_date},'
+                f'продолжительность: {duration} дней.')
+
+
 # Создайте экземпляры класса Employee с различными значениями атрибутов.
 employee1 = Employee(first_name='Роберт',
                      second_name='Крузо', gender='м')
@@ -32,9 +53,15 @@ used_days_emp1 = 7
 used_days_emp2 = 13
 
 # Допишите код для вывода информации о сотрудниках.
-print(employee1.result())
-employee1.consume_vacation(used_days_emp1)
-print(employee1.get_vacation_details())
-print(employee2.result())
-employee2.consume_vacation(used_days_emp2)
-print(employee2.get_vacation_details())
+# print(employee1.result())
+# employee1.consume_vacation(used_days_emp1)
+# print(employee1.get_vacation_details())
+# print(employee2.result())
+# employee2.consume_vacation(used_days_emp2)
+# print(employee2.get_vacation_details())
+
+# Пример использования:
+full_time_employee = FullTimeEmployee('Роберт', 'Крузо', 'м')
+print(full_time_employee.get_unpaid_vacation('2023-07-01', 5))
+part_time_employee = PartTimeEmployee('Алёна', 'Пятницкая', 'ж')
+print(part_time_employee.get_vacation_details())
