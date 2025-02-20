1df671c5fe3293b83fa69a8a078bb68286342284
Khasan Atsaev <atsaev.khasan@yandex.ru>
2025-02-20 21:44:02 +0300

    new changes

---
+++ ProductManager.py
@@ -0,0 +1,54 @@
+class Product:
+    # Опишите инициализатор класса.
+    # Инициализатор должен принять на вход
+    # название (name) и количество (quantity) товара.
+    # В теле инициализатора задайте соответствующие атрибуты экземпляра класса.
+    def __init__(
+            self,
+            name: str,
+            quantity: int):
+        self.name = name
+        self.quantity = quantity
+
+    def get_basic_info(self):
+        return f'{self.name} (в наличии: {self.quantity})'
+
+    def get_full_info(self):
+        pass
+
+
+class Kettlebell(Product):
+    # Опишите инициализатор класса и переопределите метод get_full_info()
+    def __init__(
+            self,
+            name: str,
+            quantity: int,
+            weight: float):
+        super().__init__(name, quantity)
+        self.weight = weight
+
+    def get_full_info(self):
+        return f'{self.get_basic_info()}. Вес: {self.weight} кг'
+
+
+class Clothing(Product):
+    # Опишите инициализатор класса и переопределите метод get_full_info()
+    def __init__(
+            self,
+            name: str,
+            quantity: int,
+            size: str):
+        super().__init__(name, quantity)
+        self.size = size
+
+    def get_full_info(self):
+        return f'{self.get_basic_info()}. Размер: {self.size}'
+
+
+# Для проверки вашего кода создадим пару объектов...
+small_kettlebell = Kettlebell('Гиря малая', 15, 2)
+shirt = Clothing('Футболка', 5, 'L')
+
+# ...и вызовем их методы:
+print(small_kettlebell.get_full_info())
+print(shirt.get_full_info())
